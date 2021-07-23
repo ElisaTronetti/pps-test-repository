4022276fc7f194044f88ef5d9ade12b556afabb7
Piero Sanchi <piero.sanchi@studio.unibo.it>
2021-07-23 19:32:02 +0200

    pushato scalastyle con due warning

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -4,12 +4,13 @@ object Hello extends Greeting with App {
 
   def printGreetings() {
     println(greeting)
+    println(1+0)
   }
+
   printGreetings()
 }
 
-
-
 trait Greeting {
   lazy val greeting: String = "hello"
+  println("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
 }
