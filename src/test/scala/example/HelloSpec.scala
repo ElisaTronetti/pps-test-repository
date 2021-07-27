75f71ca809937613a06bd340e32f3771f9581ce5
etronetti <etronetti@onitsanita.it>
2021-07-27 11:38:04 +0200

    Added examples of every scala test style

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -2,7 +2,15 @@ package example
 
 import org.scalatest._
 
-class HelloSpec extends FlatSpec with Matchers {
+//style FunSuite
+class Test1 extends FunSuite {
+  test("The Hello object should say hello"){
+    assert(Hello.greeting == "ciao angelo")
+  }
+}
+
+//style FlatSpec
+class Test2 extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "ciao angelo"
   }
@@ -10,5 +18,46 @@ class HelloSpec extends FlatSpec with Matchers {
   "Test " should "finish" in {
     Hello.printGreetings()
   }
+}
 
+//style FunSpec
+class Test3 extends FunSpec {
+  describe("The Hello object"){
+    describe("when checked"){
+      describe("should say hello"){
+        assert(Hello.greeting == "ciao angelo")
+      }
+    }
+  }
+}
+
+//style WordSpec
+class Test4 extends WordSpec {
+  "The Hello object" when {
+    "checked" should {
+      "say hello" in {
+        assert(Hello.greeting == "ciao angelo")
+      }
+    }
+  }
+}
+
+//style FeatureSpec
+class Test5 extends FeatureSpec with GivenWhenThen {
+  info("As the creator of the Hello object")
+  info("I want him to be respectful")
+  info("So I can call him when I want")
+  info("And he has to say hello")
+
+  feature("Check Hello object") {
+    scenario("I check when I want") {
+
+      Given("I check the Hello object")
+
+      When("When I want")
+
+      Then("He say hello")
+      assert(Hello.greeting == "ciao angelo")
+    }
+  }
 }
